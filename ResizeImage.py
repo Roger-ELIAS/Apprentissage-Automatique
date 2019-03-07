6d251dced0da26533826e320aa4c9899798c7ecf
billy-tran <billy.tran@etu.univ-amu.fr>
2019-03-07 13:52:16 +0100

    Amélioration resizeImage()

--- ResizeImage.py
+++ ResizeImage.py
@@ -2,9 +2,13 @@ import PIL
 from PIL import Image
 import os
 
-files = os.listdir("Data/MerBase")
+def resizeImage() :
 
-for fileName in files :
-    baseImage = Image.open(fileName)
-    baseImage = baseImage.resize((200,200))
-    baseImage.save("Date/Mer/"+fileName)
+    files = os.listdir("Data/MerBase")
+
+    for fileName in files :
+        baseImage = Image.open("Data/MerBase/"+fileName)
+        print(fileName)
+        baseImage = baseImage.resize((200, 200))
+        baseImage.save("Data/Mer/" + fileName)
+
