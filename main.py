92a1cd35f4762bb73b182d8ee004d72e537aebc2
billy-tran <billy.tran@etu.univ-amu.fr>
2019-03-07 15:40:25 +0100

    Modification des fonctions generateMatrixTrain et ajout d'une autre fonction generateMatrixTest

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import generateMatrix
+import learn
+import numpy
 
-final = generateMatrix.generateMatrix()
-print(len(final[1]))
+def diff(a, b):
+    target = numpy.array(a)
+    result = numpy.array(b)
+    error = numpy.mean(target != result)
+    return round(100 - error * 100, 1)
 
 
+if __name__ == '__main__':
+    final = generateMatrix.generateMatrixTrain("Data")
 
+    result = learn.training(final)
+
+    print("Taux de réussite : ", diff(result, final[1]))
