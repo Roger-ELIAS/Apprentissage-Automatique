16b63ee24c96ca0cb37242c9f5af989bceab16d7
Romain Colonna D'istria <rom@MacBook-Air-de-Romain.local>
2019-03-14 14:02:15 +0100

    rename folder

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def diff(a, b):
     target = numpy.array(a)
     result = numpy.array(b)
     error = numpy.mean(target != result)
-    return round(100 - error * 100, 1)
+    return round(100 - error * 100, 10)
 
 
 if __name__ == '__main__':
-    final = generateMatrix.generateMatrixTrain("Data")
+    final = generateMatrix.generateMatrixTrain("DATA_TRAIN_FOLDER")
     result = learn.training(final)
 
     print("Taux de réussite : ", diff(result, final[1]))
