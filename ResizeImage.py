cb0126c05e0265ab5f35eb2dc82aadef34fc9eef
Roger-ELIAS <roger.elias@etu.univ-amu.fr>
2019-03-14 15:20:53 +0100

    Rotation des images

--- ResizeImage.py
+++ ResizeImage.py
@@ -20,5 +20,31 @@ def resizeImage() :
         baseImage = baseImage.resize((200, 200))
         baseImage.save("Data/Ailleurs/" + fileName)
 
+def rotateImage():
 
-resizeImage()
+    files = os.listdir("Data/Mer")
+
+    for fileName in files :
+        baseImage = Image.open("Data/Mer/"+fileName)
+        nb = 36
+        test = fileName.split(".")
+        while nb < 360:
+            newImage = baseImage.rotate(nb,Image.BICUBIC, True)
+            newfile = test[0] + str(nb) + "." +test[1]
+            nb = nb+36
+            newImage.save("RotationImage/" + newfile)
+
+    files = os.listdir("Data/Ailleurs")
+
+    for fileName in files:
+        baseImage = Image.open("Data/Ailleurs/" + fileName)
+        nb = 36
+        test = fileName.split(".")
+        while nb < 360:
+            newImage = baseImage.rotate(nb, Image.BICUBIC, True)
+            newfile = test[0] + str(nb) + "." + test[1]
+            nb = nb + 36
+            newImage.save("RotationImage/" + newfile)
+
+
+#rotateImage()
