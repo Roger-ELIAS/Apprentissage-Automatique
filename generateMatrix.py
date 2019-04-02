ef3823211eb5004ad94e2e09d87beed38d5afbcd
Roger-ELIAS <roger.elias@etu.univ-amu.fr>
2019-04-02 13:17:29 +0200

    Commit mise à jour

--- generateMatrix.py
+++ generateMatrix.py
@@ -4,43 +4,107 @@ import resizeImage
 
 
 def getHistogram(fileName) :
-    baseImage = Image.open(fileName)
-    return baseImage.histogram()
+	return fileName.histogram()
 
 def generateMatrixTrain(foldername) :
-    matrix = []
-    vector = []
+	matrix = []
+	vector = []
 
-    files = os.listdir(foldername +"/Mer")
+	files = os.listdir(foldername +"/Mer/")
 
-    for fileName in files :
-        baseImage = Image.open(foldername +"/Mer"+fileName)
-        imageResize = resizeImage.resizeImage(baseImage)
-        histo = getHistogram(imageResize)
-        matrix.append(histo)
-        vector.append(1)
+	for fileName in files:
+		baseImage = Image.open(foldername +"/Mer/"+fileName)
+		imageResize = resizeImage.resizeImage(baseImage)
+		histo = getHistogram(imageResize)
+		matrix.append(histo)
+		vector.append(1)
+		
+		for rotationPhase in range(0,3):
+			for deg in range(1,3) :
+				imageRotate = resizeImage.rotateSingleImage(imageResize, rotationPhase * 90 + deg)
+				histo = getHistogram(imageRotate)
+				matrix.append(histo)
+				vector.append(1)
+				
 
-    files = os.listdir(foldername +"/Ailleurs")
-    for fileName in files:
-        baseImage = Image.open(foldername +"/Ailleurs"+fileName)
-        imageResize = resizeImage.resizeImage(baseImage)
-        histo = getHistogram(imageResize)
-        matrix.append(histo)
-        vector.append(-1)
 
-    return [matrix,vector]
+				imageRotate = resizeImage.rotateSingleImage(imageResize, rotationPhase * 90 - deg)
+				histo = getHistogram(imageRotate)
+				matrix.append(histo)
+				vector.append(1)
+				
+
+
+			imageRotate = resizeImage.rotateSingleImage(imageResize, (rotationPhase + 1) * 90)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(1)
+			
+
+			
+		for deg in range(1,3) :
+			imageRotate = resizeImage.rotateSingleImage(imageResize, 270 + deg)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(1)
+
+			imageRotate = resizeImage.rotateSingleImage(imageResize, 270 - deg)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(1)
+			
+
+	files = os.listdir(foldername +"/Ailleurs/")
+	for fileName in files:
+		baseImage = Image.open(foldername +"/Ailleurs/"+fileName)
+		imageResize = resizeImage.resizeImage(baseImage)
+		histo = getHistogram(imageResize)
+		matrix.append(histo)
+		vector.append(-1)
+		
+		for rotationPhase in range(0,3) :
+			for deg in range(1,3) :
+				imageRotate = resizeImage.rotateSingleImage(imageResize, rotationPhase * 90 + deg)
+				histo = getHistogram(imageRotate)
+				matrix.append(histo)
+				vector.append(-1)
+
+				imageRotate = resizeImage.rotateSingleImage(imageResize, rotationPhase * 90 - deg)
+				histo = getHistogram(imageRotate)
+				matrix.append(histo)
+				vector.append(-1)
+
+
+			imageRotate = resizeImage.rotateSingleImage(imageResize, (rotationPhase + 1) * 90)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(-1)
+
+		for deg in range(1,3) :
+			imageRotate = resizeImage.rotateSingleImage(imageResize, 270 + deg)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(-1)
+
+			imageRotate = resizeImage.rotateSingleImage(imageResize, 270 - deg)
+			histo = getHistogram(imageRotate)
+			matrix.append(histo)
+			vector.append(-1)
+
+	return [matrix,vector]
 
 def generateMatrixTest(folder) :
-    matrix = []
+	matrix = []
+	
+	file = folder + "/"
+	files = os.listdir(file)
+	
+	for fileName in files :
+		baseImage = Image.open(file+fileName)
+		imageResize = resizeImage.resizeImage(baseImage)
+		histo =  imageResize.histogram()
+		matrix.append(histo)
 
-    files = os.listdir(folder)
+	return matrix
 
-
-    for fileName in files :
-
-        histo = getHistogram(folder+ "/" + fileName)
-        matrix.append(histo)
-
-    return matrix
-
-generateMatrixTrain("Data")
+#generateMatrixTrain("Data")
--- resizeImage.py
+++ resizeImage.py
@@ -3,35 +3,9 @@ from PIL import Image
 import os
 
 def resizeImage(image) :
-    Image._show(image)
     baseImage = image.resize((200, 200))
-    Image._show(baseImage)
     return baseImage
 
-def rotateImage():
-
-    files = os.listdir("Data/Mer")
-
-    for fileName in files :
-        baseImage = Image.open("Data/Mer/"+fileName)
-        nb = 36
-        name = fileName.split(".")
-        while nb < 360:
-            newImage = baseImage.rotate(nb,Image.BICUBIC, True)
-            newfile = name[0] + str(nb) + "." + name[1]
-            nb = nb+36
-            newImage.save("Data/Mer/" + newfile)
-
-    files = os.listdir("Data/Ailleurs")
-
-    for fileName in files:
-        baseImage = Image.open("Data/Ailleurs/" + fileName)
-        nb = 36
-        test = fileName.split(".")
-        while nb < 360:
-            newImage = baseImage.rotate(nb, Image.BICUBIC, True)
-            newfile = test[0] + str(nb) + "." + test[1]
-            nb = nb + 36
-            newImage.save("Data/Ailleurs/" + newfile)
-
-rotateImage()
+def rotateSingleImage(baseImage, deg) :
+    newImage = baseImage.rotate(deg, Image.BICUBIC, True)
+    return newImage
