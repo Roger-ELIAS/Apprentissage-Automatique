fe279406db884312e9859eba6412fe1d8bb7c544
Vincent Mazel <vincent@LAPTOP-GNDV3A9O.localdomain>
2019-04-02 18:32:02 +0200

    generateMatrix factorisé

--- generateMatrix.py
+++ generateMatrix.py
@@ -13,54 +13,57 @@ def rotateImage(baseImage, deg) :
     newImage = baseImage.rotate(deg, Image.BICUBIC, True)
     return newImage
 
-def append(histo, classe):
+def append(histo, classe) :
         matrix.append(histo)
         vector.append(classe)
 
 def getHistogram(fileName) :
 	return fileName.histogram()
 
-def rotateSingleImage(imageResize, rotationDegre, classe):
+def rotateSingleImage(imageResize, rotationDegre, classe) :
 	imageRotate = rotateImage(imageResize, rotationDegre)
 	histo = getHistogram(imageRotate)
 	append(histo, classe)
+
+def rotateImages(images, deg, classe) :
+    for image in images :
+        rotateSingleImage(image, deg, classe)
+
 
 def rotate(folder, classe):
         files = os.listdir(folder)
 
         for fileName in files:
                 baseImage = Image.open(folder + fileName)
+                images = []
+
                 imageResize = resizeImage(baseImage)
                 imageFlipLR = imageResize.transpose(Image.FLIP_LEFT_RIGHT)
                 imageFlipTB = imageResize.transpose(Image.FLIP_TOP_BOTTOM)
+
+                images.append(imageResize)
+                images.append(imageFlipLR)
+                images.append(imageFlipTB)
+
                 histo = imageResize.histogram()
                 append(histo, classe)
+
                 histo = imageFlipLR.histogram()
                 append(histo, classe)
+
                 histo = imageFlipTB.histogram()
                 append(histo, classe)
 
- 
                 for rotationPhase in range(0,3):
                         for deg in range(1,3) :
-                                rotateSingleImage(imageResize, rotationPhase * 90 + deg, classe)
-                                rotateSingleImage(imageFlipLR, rotationPhase * 90 + deg, classe)
-                                rotateSingleImage(imageFlipTB, rotationPhase * 90 + deg, classe)
-                                rotateSingleImage(imageResize, rotationPhase * 90 - deg, classe)
-                                rotateSingleImage(imageFlipLR, rotationPhase * 90 - deg, classe)
-                                rotateSingleImage(imageFlipTB, rotationPhase * 90 - deg, classe)
+                                rotateImages(images, rotationPhase * 90 + deg, classe)
+                                rotateImages(images, rotationPhase * 90 - deg, classe)
 
-                        rotateSingleImage(imageResize, (rotationPhase + 1) * 90, classe)
-                        rotateSingleImage(imageFlipLR, (rotationPhase + 1) * 90, classe)
-                        rotateSingleImage(imageFlipTB, (rotationPhase + 1) * 90, classe)
+                        rotateImages(images, (rotationPhase + 1) * 90, classe)
                             
                 for deg in range(1,3) :
-                        rotateSingleImage(imageResize, 270 + deg, classe)
-                        rotateSingleImage(imageFlipLR, 270 + deg, classe)
-                        rotateSingleImage(imageFlipTB, 270 + deg, classe)
-                        rotateSingleImage(imageResize, 270 - deg, classe)
-                        rotateSingleImage(imageFlipLR, 270 - deg, classe)
-                        rotateSingleImage(imageFlipTB, 270 - deg, classe)
+                        rotateImages(images, 270 + deg, classe)
+                        rotateImages(images, 270 - deg, classe)
 
 def generateMatrixTrain(foldername) :
 	rotate(foldername + "/Mer/", 1)
